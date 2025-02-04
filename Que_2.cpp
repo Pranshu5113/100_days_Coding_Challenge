73e93ad63711fa55dfb167d7cc91d027820fc3e8
Pranshu Singh Patel <116775668+Pranshu5113@users.noreply.github.com>
2025-02-04 22:25:28 +0530

    Add files via upload

---
+++ Que_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int num;
+
+    cout<<"Enter the Number: ";
+    cin>>num;
+
+    if(num%2 == 0){
+        cout<< "Even";
+    }
+    else{
+        cout<<"Odd";
+    }
+    return 0;
+}
