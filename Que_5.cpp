5d756f66f62c9491ae96ce99401c923635d56c97
Pranshu Singh Patel <116775668+Pranshu5113@users.noreply.github.com>
2025-02-07 18:32:25 +0530

    Add files via upload

---
+++ Que_5.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b;
+
+    cout<<"Enter the Value of a: ";
+    cin>>a;
+
+    cout<<"Enter the Value of b: ";
+    cin>>b;
+
+    if (a>b) {
+
+        cout<<"a is Greater";
+    }
+    else
+        cout<<"b is Greater";
+
+    return 0;
+
+}
